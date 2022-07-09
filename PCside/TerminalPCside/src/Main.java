fbdd82174c7c96a0fadbc2e5ea33564abc657f27
barryyosilevich <barry.yosilevich@whitesourcesoftware.com>
2022-07-09 18:16:58 +0300

    Added basic communication infrastructure

---
+++ PCside/TerminalPCside/src/Main.java
@@ -0,0 +1,7 @@
+import com.fazecast.jSerialComm.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ PCside/TerminalProject/src/main/java/Terminal.java
@@ -0,0 +1,73 @@
+import com.fazecast.jSerialComm.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class Terminal {
+    private enum State {Sleep, Chat, FileTransfer, TerminalConfig }
+    private static State sysState = State.Sleep;
+    private static SerialPort sysPort;
+
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    // TODO - finalize this method.
+    public static SerialPort getSerialPort(){
+        final SerialPort[] availablePorts = SerialPort.getCommPorts();
+
+        for (SerialPort port : availablePorts)
+            System.out.println(port.getDescriptivePortName());
+
+        sysPort = availablePorts[0];
+        //        for (SerialPort port : ports){
+//            if(port.friendlyName == "UARTXXXSHIT"){
+//
+//            }
+//        }
+//          https://fazecast.github.io/jSerialComm/
+
+        return availablePorts[0];
+    }
+
+    // TODO - Finalize this method.
+    public static void initNewSerialPort(String name, int baud, int dataBits, int stopBits){
+        if (sysPort != null && sysPort.isOpen()) {
+            closePort();
+        }
+        sysPort = SerialPort.getCommPort(name);
+        sysPort.setParity(SerialPort.NO_PARITY);
+        sysPort.setNumStopBits(stopBits);
+        sysPort.setNumDataBits(dataBits);
+        sysPort.setBaudRate(baud);
+        sysPort.addDataListener(this);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        final SerialPort port = getSerialPort();
+        initNewSerialPort(port.getDescriptivePortName(), 9600, 8,1);
+         
+        while(true){
+
+            switch (sysState){
+
+                case Chat: {
+                    // TODO - implement
+                    sysPort.serialRead();
+                }break;
+
+                case FileTransfer:
+                    // TODO - implement
+
+                case TerminalConfig:
+                {
+                    // TODO - pick port using gui
+                    // TODO - set baudRate using gui - port.setBaudRate(baud);
+                    sysState = State.Sleep;
+                }break;
+            }
+        }
+
+    }
+}
